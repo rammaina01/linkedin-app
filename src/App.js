fe464d75581a0def018166cb21cacff767b26c25
“rammaina01” <ram.b.maina01@gmail.com>
2021-12-26 00:26:31 +0530

    Adding react hooks

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import Feed from "./components/feed/Feed";
+import Hooks from "./react-hooks/Hooks";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* APP body */}
-      <div className="app__body">
-        {/* sidebar */}
-        <Sidebar />
+    // <div className="app">
+    //   {/* Header */}
+    //   <Header />
+    //   {/* APP body */}
+    //   <div className="app__body">
+    //     {/* sidebar */}
+    //     <Sidebar />
+    //     {/* Feed */}
+    //     <Feed />
+    //     {/* Widgets */}
+    //   </div>
+    //</div>
 
-        {/* Feed */}
-        <Feed />
-
-        {/* Widgets */}
-      </div>
+    <div className="app" >
+      <Hooks></Hooks>
     </div>
+
   );
 }
 
